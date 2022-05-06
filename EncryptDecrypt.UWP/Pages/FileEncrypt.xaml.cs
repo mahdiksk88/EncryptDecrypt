7836e66fb1adb986ec154290f0506c2404fd142a
mahdi kashkalani <mahdiksk88@outlook.com>
2022-05-06 23:50:13 +0430

    Added main navigation view and pages for surther uses

--- EncryptDecrypt.UWP/Pages/FileEncrypt.xaml.cs
+++ EncryptDecrypt.UWP/Pages/FileEncrypt.xaml.cs
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace EncryptDecrypt.UWP
+namespace EncryptDecrypt.UWP.Pages
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class FileEncrypt : Page
     {
-        public MainPage()
+        public FileEncrypt()
         {
             this.InitializeComponent();
-        }
-
-        private void AppFrame_Navigated(object sender, NavigationEventArgs e)
-        {
-            MainNav.IsBackEnabled = AppFrame.CanGoBack;
         }
     }
 }
